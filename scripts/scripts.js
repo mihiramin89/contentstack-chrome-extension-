09d5a22747a80c0818cffb039f98f9df5f952f15
Mihir Amin <mihiramin89@gmail.com>
2016-11-22 11:58:20 -0500

    add hover bubble with tagname & tag classname or id if exists

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,12 @@ if(divExists === null){
     var overlaydiv = document.createElement("div");
     overlaydiv.id = "highlight";
     document.body.appendChild(overlaydiv);
-
+    var hoverDiv = document.createElement("div");
+    hoverDiv.className = "hoverBox";
+    var internalDiv = document.createElement("div");
+    internalDiv.className = "hoverBox-internal";
+    overlaydiv.appendChild(internalDiv);
+    overlaydiv.appendChild(hoverDiv);
 }
 
 var no = [document.body, document.documentElement, document];
@@ -13,6 +18,8 @@ var no = [document.body, document.documentElement, document];
 document.body.addEventListener("mousemove", function(e) {
 
     var overlay = document.getElementById("highlight");
+    var hoverBox = document.getElementsByClassName("hoverBox")[0];
+    var hoverText = document.getElementsByClassName("hoverBox-internal")[0];
     if (e.target === cur) {
         return;
     }
@@ -20,6 +27,7 @@ document.body.addEventListener("mousemove", function(e) {
     if (~no.indexOf(e.target)) {
         cur = null;
         overlay.style.display = 'none';
+        hoverBox.style.display = 'none';
         return;
     }
 
@@ -35,4 +43,18 @@ document.body.addEventListener("mousemove", function(e) {
     overlay.style.left = offsetLeft + "px";
     overlay.style.width = width + "px";
     overlay.style.height = height + "px";
+
+
+    var tagInfo = e.target.nodeName.toLowerCase();
+    var targetClassName = e.target.className;
+    var targetID = e.target.id;
+    if(targetClassName !== null && targetClassName !== undefined && targetClassName !== ""){
+        tagInfo += "." + targetClassName;
+    }
+    if(targetID !== null && targetID !== undefined && targetID !== "") {
+        tagInfo += "#" + targetID;
+    }
+    
+    hoverBox.innerHTML = tagInfo;
+
 });
